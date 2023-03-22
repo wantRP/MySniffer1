ed1360165c2fc96553f74d62e1471ea7ed1b8c27
天畅 赵 <duchuan1024@hotmail.com>
2023-03-22 22:36:17 +0800

    Pause and Stop

--- Model/Packet.cs
+++ Model/Packet.cs
@@ -10,12 +10,14 @@ namespace MySniffer1.Model {
 	public class RawPacket {
 		public RawCapture p;
 		public int Number { get; private set; }
-		public PosixTimeval Timeval { get { return p.Timeval; } }
+		//public PosixTimeval Timeval { get { return p.Timeval; } }
 		//public LinkLayers LinkLayerType { get { return p.LinkLayerType; } }
 		public int Length { get { return p.Data.Length; } }
 		public string Source { get; set; }
 		public string Destination { get; set; }
+		public string strr { get; set; }
 		public byte[] Data { get; set; }
+		public string Base64 { get { return Convert.ToBase64String(Data); } }
 		private TimeSpan time;
 		public string Time { get { return time.ToString("hh':'mm':'ss'.'ff"); } }
 		public RawPacket(RawCapture p, int number) {
@@ -23,6 +25,7 @@ namespace MySniffer1.Model {
 			this.Number = number;
 			this.time = DateTime.Now.TimeOfDay;
 			Packet packet = Packet.ParsePacket(p.LinkLayerType, p.Data);
+			strr=packet.ToString(StringOutputType.Colored);
 			this.Data = p.Data;
 		}
 
